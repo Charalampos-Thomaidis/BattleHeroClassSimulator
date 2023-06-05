64c0ed5bfbf2f4ae2b709218f81227b8e8626c34
Charalampos-Thomaidis <risxapsyy@gmail.com>
2023-06-05 22:54:35 +0300

    Add program

---
+++ BattleHeroClassSimulator.cpp
@@ -0,0 +1,159 @@
+#include "BattleHeroClassSimulator.h"
+
+int main() 
+{
+    // Welcome the user to the program
+    std::cout << "Welcome to the Battle Hero Simulator!" << std::endl;
+    std::cout << "Version: 2.0" << std::endl;
+    std::cout << "Created by: Charalampos Thomaidis" << std::endl;
+
+    int choice;
+    do 
+    {
+        //Main Menu
+        std::cout << std::endl << "Main Menu: \n" << std::endl;
+        std::cout << "1. Help \n";
+        std::cout << "2. Start \n";
+        std::cout << "3. Exit \n" << std::endl;
+        std::cout << "~Enter your choice: ";
+        choice = getPlayerChoice(1, 3);
+
+        // If choice is 1 then display on console the help information for the program
+        if (choice == 1) 
+        {
+            std::cout << std::endl << "~Help: This program simulates a battle between two players. \n"
+                "~Each player has a name, class, and weapon. \n"
+                "~The battle proceeds with turns alternating between the players. \n"
+                "~The player who reduces the other player's health to 0 wins the battle. \n"
+                "~The player's attack power, defense rating and health are randomly generated \n"
+                "~based on their class and weapon choices. \n"
+                "~The program will always save the battles into a Score.dat file, \n"
+                "~and also ask the user if he wants to print the score after the battle. \n";
+        }
+
+        // If choice is 2 start the round
+        else if (choice == 2) 
+        {
+            std::string player1Name;
+            std::string player2Name;
+            PlayerClass player1Class;
+            PlayerClass player2Class;
+            Item player1Item;
+            Item player2Item;
+
+            // Player 1 Name , Class , Weapon choices
+            std::cout << std::endl << "~Enter Player 1 name: ";
+            std::cin.ignore();
+            getline(std::cin, player1Name);
+
+            std::cout << std::endl << "~Choose your class for " << player1Name << "\n" << std::endl;
+            std::cout << "1: Warrior" << std::endl << "2: Archer" << std::endl << "3: Rogue" << std::endl << "4: Mage \n" << std::endl;
+            std::cout << "~Enter your choice: ";
+            int player1ClassChoice = getPlayerChoice(1, 4);
+            player1Class = static_cast<PlayerClass>(player1ClassChoice - 1);
+
+            std::cout << std::endl << "~Choose your weapon for " << player1Name << "\n" << std::endl;
+            std::cout << "1: Sword" << std::endl << "2: Bow" << std::endl << "3: Dagger" << std::endl << "4: Staff \n" << std::endl;
+            std::cout << "~Enter your choice: ";
+            int player1ItemChoice = getPlayerChoice(1, 4);
+            player1Item = static_cast<Item>(player1ItemChoice - 1);
+
+            // Player 2 Name , Class , Weapon choices
+            std::cout << std::endl << "~Enter Player 2 name: ";
+            std::cin.ignore();
+            getline(std::cin, player2Name);
+
+            std::cout << std::endl << "~Choose your class for " << player2Name << "\n" << std::endl;
+            std::cout << "1: Warrior" << std::endl << "2: Archer" << std::endl << "3: Rogue" << std::endl << "4: Mage \n" << std::endl;
+            std::cout << "~Enter your choice: ";
+            int player2ClassChoice = getPlayerChoice(1, 4);
+            player2Class = static_cast<PlayerClass>(player2ClassChoice - 1);
+
+            std::cout << std::endl << "~Choose your weapon for " << player2Name << "\n" << std::endl;
+            std::cout << "1: Sword" << std::endl << "2: Bow" << std::endl << "3: Dagger" << std::endl << "4: Staff \n" << std::endl;
+            std::cout << "~Enter your choice: ";
+            int player2ItemChoice = getPlayerChoice(1, 4);
+            player2Item = static_cast<Item>(player2ItemChoice - 1);
+
+            // Create players
+            Player player1(player1Name, player1Class, player1Item);
+            Player player2(player2Name, player2Class, player2Item);
+
+            // Display player information
+            std::cout << std::endl << "Player 1:" << std::endl;
+            displayPlayer(player1);
+            std::cout << "Player 2:" << std::endl;
+            displayPlayer(player2);
+
+            int turnNumber = 1;
+            while (player1.getHealth() > 0 && player2.getHealth() > 0) 
+            {
+                std::cout << std::endl << "~Turn " << turnNumber << ":" << std::endl;
+
+                // Randomly determine which player attacks first
+                int firstPlayer = player1.getRandomNumber(1, 2);
+                int secondPlayer = (firstPlayer == 1) ? 2 : 1;
+
+                // Player 1 attacks
+                if (firstPlayer == 1) 
+                {
+                    std::cout << player1.getName() << " (Player 1) attacks " << player2.getName() << " (Player 2)!" << std::endl;
+                    int damage = player1.attack(player2);
+                    std::cout << "Damage dealt: " << damage << std::endl;
+                    std::cout << player2.getName() << "'s remaining health: " << player2.getHealth() << std::endl;
+                }
+
+                // Player 2 attacks
+                else 
+                {
+                    std::cout << player2.getName() << " (Player 2) attacks " << player1.getName() << " (Player 1)!" << std::endl;
+                    int damage = player2.attack(player1);
+                    std::cout << "Damage dealt: " << damage << std::endl;
+                    std::cout << player1.getName() << "'s remaining health: " << player1.getHealth() << std::endl;
+                }
+
+                turnNumber++;
+            }
+
+            // Determine the winner
+            std::cout << std::endl << "~Battle is over!" << std::endl;
+            std::string winner;
+
+            if (player1.getHealth() > 0)
+            {
+                winner = player1.getName() + " (Player 1) " + player1.getClassString() + " (Class) " + player1.getItemString() + " (Weapon)";
+                std::cout << winner << " wins the battle! \n" << std::endl;
+            }
+            else if (player2.getHealth() > 0)
+            {
+                winner = player2.getName() + " (Player 2) " + player2.getClassString() + " (Class) " + player2.getItemString() + " (Weapon) ";
+                std::cout << winner << " wins the battle! \n" << std::endl;
+            }
+            else
+            {
+                winner = "It's a tie!";
+                std::cout << winner << std::endl;
+            }
+
+            // Save the results to the file
+            saveResultsToFile(player1.getName(), player2.getName(), winner);
+
+            // Ask if the user wants to print the results to the console
+            std::cout << "~Do you want to print the results to the console? (Yes/No): \n" << std::endl;
+            std::string printChoice;
+            std::cout << "~Enter your choice: ";
+            std::cin >> printChoice;
+            std::cout << std::endl;
+
+            if (printChoice == "Yes" || printChoice == "yes")
+            {
+                std::cout << "Player1: " << player1.getName() << std::endl;
+                std::cout << "Player2: " << player2.getName() << std::endl;
+                std::cout << "Winner: " << winner << std::endl;
+            }
+        }
+    } 
+    while (choice != 3);
+
+    return 0;
+}
---
+++ BattleHeroClassSimulator.h
@@ -0,0 +1,264 @@
+#ifndef BattleHeroClassSimulator_H
+#define BattleHeroClassSimulator_H
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <fstream>
+#include <limits>
+
+enum class PlayerClass 
+{
+	Warrior,
+	Archer,
+	Rogue,
+	Mage
+};
+
+enum class Item 
+{
+	Sword,
+	Bow,
+	Dagger,
+	Staff
+};
+
+class Player 
+{
+public:
+	Player(const std::string& playerName, PlayerClass playerClass, Item playerItem)
+		: name(playerName), heroClass(playerClass), item(playerItem)
+	{
+		health = generateRandomNumber(800, 1000);
+		attack_power = calculateAttackPower();
+		defense_rating = calculateDefenseRating();
+	}
+
+	// Calculate the damage from the Player when attacks the otherPlayer
+	int attack(Player& otherPlayer)
+	{
+		float r1 = generateRandomFloat(0.0f, 1.0f);
+		float r2 = generateRandomFloat(0.0f, 1.0f);
+		float a = attack_power * r1;
+		float d = 1.0f - (otherPlayer.defense_rating * r2);
+		int damage = static_cast<int>(a * d);
+		otherPlayer.health -= damage;
+		return damage;
+	}
+
+	std::string getName() const 
+	{
+		return name;
+	}
+
+	std::string getClassString() const
+	{
+		switch (heroClass)
+		{
+		case PlayerClass::Warrior:
+			return "Warrior";
+		case PlayerClass::Archer:
+			return "Archer";
+		case PlayerClass::Rogue:
+			return "Rogue";
+		case PlayerClass::Mage:
+			return "Mage";
+		}
+	}
+
+	std::string getItemString() const
+	{
+		switch (item)
+		{
+		case Item::Sword:
+			return "Sword";
+		case Item::Bow:
+			return "Bow";
+		case Item::Dagger:
+			return "Dagger";
+		case Item::Staff:
+			return "Staff";
+		}
+	}
+
+	int getHealth() const
+	{
+		return health;
+	}
+
+	int getAttackPower() const
+	{
+		return attack_power;
+	}
+
+	float getDefenseRating() const
+	{
+		return defense_rating;
+	}
+
+	int getRandomNumber(int min, int max)
+	{
+		return generateRandomNumber(min, max);
+	}
+
+private:
+	std::string name;
+	PlayerClass heroClass;
+	Item item;
+	int health;
+	int attack_power;
+	float defense_rating;
+
+	// Generate random int
+	int generateRandomNumber(int min, int max)
+	{
+		static std::random_device rd;
+		static std::mt19937 rng(rd());
+		std::uniform_int_distribution<int> distribution(min, max);
+		return distribution(rng);
+	}
+
+	// Generate random float
+	float generateRandomFloat(float min, float max)
+	{
+		static std::random_device rd;
+		static std::mt19937 rng(rd());
+		std::uniform_real_distribution<float> distribution(min, max);
+		return distribution(rng);
+	}
+
+	// Calculate the attack power of the player depending which class, the stats for each class are generated randomly
+	int calculateAttackPower()
+	{
+		int baseAttackPower = 0;
+		switch (heroClass)
+		{
+		case PlayerClass::Warrior:
+			baseAttackPower = generateRandomNumber(150, 300);
+			break;
+		case PlayerClass::Archer:
+			baseAttackPower = generateRandomNumber(100, 250);
+			break;
+		case PlayerClass::Rogue:
+			baseAttackPower = generateRandomNumber(120, 280);
+			break;
+		case PlayerClass::Mage:
+			baseAttackPower = generateRandomNumber(80, 200);
+			break;
+		}
+		return static_cast<int>(baseAttackPower * getItemMultiplier());
+	}
+
+	// Calculate the defense rating of the player depending which class, the stats for each class are generated randomly
+	float calculateDefenseRating()
+	{
+		float baseDefenseRating = 0.0f;
+		switch (heroClass)
+		{
+		case PlayerClass::Warrior:
+			baseDefenseRating = generateRandomFloat(0.5f, 0.8f);
+			break;
+		case PlayerClass::Archer:
+			baseDefenseRating = generateRandomFloat(0.3f, 0.6f);
+			break;
+		case PlayerClass::Rogue:
+			baseDefenseRating = generateRandomFloat(0.4f, 0.7f);
+			break;
+		case PlayerClass::Mage:
+			baseDefenseRating = generateRandomFloat(0.2f, 0.5f);
+			break;
+		}
+		return baseDefenseRating;
+	}
+
+	// Get the item multiplier from the weapon to add to the attack power of the player
+	float getItemMultiplier() const
+	{
+		float multiplier = 1.0f;
+		switch (item)
+		{
+		case Item::Sword:
+			multiplier = 1.2f;
+			break;
+		case Item::Bow:
+			multiplier = 1.1f;
+			break;
+		case Item::Dagger:
+			multiplier = 1.15f;
+			break;
+		case Item::Staff:
+			multiplier = 1.05f;
+			break;
+		}
+		return multiplier;
+	}
+};
+
+// Function which displays the stats of the player (name, class, weapon, health, attack and defense rate)
+void displayPlayer(const Player& player)
+{
+	std::cout << "Name: " << player.getName() << std::endl;
+	std::cout << "Class: " << player.getClassString() << std::endl;
+	std::cout << "Item: " << player.getItemString() << std::endl;
+	std::cout << "Health: " << player.getHealth() << std::endl;
+	std::cout << "Attack Power: " << player.getAttackPower() << std::endl;
+	std::cout << "Defense Rating: " << player.getDefenseRating() << std::endl;
+	std::cout << std::endl;
+}
+
+// Function which gets the number choice of the player and checks for possible input errors
+int getPlayerChoice(int min, int max)
+{
+	int choice;
+	while (true)
+	{
+		if (std::cin >> choice)
+		{
+			if (choice >= min && choice <= max)
+			{
+				if (std::cin.peek() != '\n')
+				{
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::cout << std::endl << "~Invalid choice. Please enter a single number. \n" << std::endl;
+					std::cout << "~Enter your choice: ";
+				}
+				else
+				{
+					return choice;
+				}
+			}
+			else
+			{
+				std::cout << std::endl << "~Invalid choice. Try selecting a value between " << min << " and " << max << ". \n" << std::endl;
+				std::cout << "~Enter your choice: ";
+			}
+		}
+		else
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << std::endl << "~Invalid choice. Please enter a number. \n" << std::endl;
+			std::cout << "~Enter your choice: ";
+		}
+	}
+}
+
+// Function which saves the names , classes and weapons of both players but also who won the battle to a file named Score.dat
+void saveResultsToFile(const std::string& player1Name, const std::string& player2Name, const std::string& winner)
+{
+	std::ofstream file("Score.dat", std::ios::app);
+	if (file.is_open())
+	{
+		file << "Player 1: " << player1Name << std::endl;
+		file << "Player 2: " << player2Name << std::endl;
+		file << "Winner: " << winner << std::endl;
+		file << std::endl;
+		file.close();
+	}
+	else
+	{
+		std::cout << "~Unable to open the file for saving the results." << std::endl;
+	}
+}
+#endif // BattleHeroClassSimulator_H
